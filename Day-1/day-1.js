8fb622507772758fb53c5bee61f9f4c6991e812f
shihab-63 <thecodzhub@gmail.com>
2026-04-06 18:22:29 +0600

    Start JS problem sloving Day by day

---
+++ Day-1/day-1.js
@@ -0,0 +1,20 @@
+//* --------------------------------------------------
+//* Programming Question: Longest Word in a String
+//* --------------------------------------------------
+
+//? Q: Write a function findLongestWord that takes a string as input and returns the longest word in the string. If there are multiple longest words, return the first one encountered.
+
+//* Constraints:
+//? The input string may contain alphabetic characters, digits, spaces, and punctuation.
+//? The input string is non-empty.
+//? The input string may contain multiple words separated by spaces.
+
+//* Note:
+//? If the input string is empty or contains only whitespace, the function should return false.
+//? The function should ignore leading and trailing whitespace when determining the longest word.
+
+
+
+console.log(
+  findLongestWord("Watch Thapa Technical javascript course on youtube"),
+);
